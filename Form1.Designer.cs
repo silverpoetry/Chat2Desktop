74f849c8547a1978358ff57ebc7208002c0ca1e7
霜冷长河 <lwc19@mails.tsinghua.edu.cn>
2019-10-13 22:51:01 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Chat2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+            System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectBySelectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectByAddressToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showRadioInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new System.Drawing.Size(339, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new System.Drawing.Size(339, 6);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(784, 598);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxInput.Location = new System.Drawing.Point(0, 641);
+            this.textBoxInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(784, 35);
+            this.textBoxInput.TabIndex = 2;
+            this.textBoxInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxInput_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(784, 43);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectBySelectToolStripMenuItem,
+            this.connectByAddressToolStripMenuItem,
+            this.disconnectToolStripMenuItem,
+            toolStripMenuItem1,
+            this.showRadioInfoToolStripMenuItem,
+            toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(93, 35);
+            this.menuToolStripMenuItem.Text = "&Menu";
+            // 
+            // connectBySelectToolStripMenuItem
+            // 
+            this.connectBySelectToolStripMenuItem.Name = "connectBySelectToolStripMenuItem";
+            this.connectBySelectToolStripMenuItem.Size = new System.Drawing.Size(342, 38);
+            this.connectBySelectToolStripMenuItem.Text = "&Connect by Select";
+            this.connectBySelectToolStripMenuItem.Click += new System.EventHandler(this.menuItemConnectBySelect_Click);
+            // 
+            // connectByAddressToolStripMenuItem
+            // 
+            this.connectByAddressToolStripMenuItem.Name = "connectByAddressToolStripMenuItem";
+            this.connectByAddressToolStripMenuItem.Size = new System.Drawing.Size(342, 38);
+            this.connectByAddressToolStripMenuItem.Text = "Connect by &Address";
+            this.connectByAddressToolStripMenuItem.Click += new System.EventHandler(this.menuItemConnectByAddress_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(342, 38);
+            this.disconnectToolStripMenuItem.Text = "&Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.menuItemDisconnect_Click);
+            // 
+            // showRadioInfoToolStripMenuItem
+            // 
+            this.showRadioInfoToolStripMenuItem.Name = "showRadioInfoToolStripMenuItem";
+            this.showRadioInfoToolStripMenuItem.Size = new System.Drawing.Size(342, 38);
+            this.showRadioInfoToolStripMenuItem.Text = "&Radio info";
+            this.showRadioInfoToolStripMenuItem.Click += new System.EventHandler(this.menuItemShowRadioInfo_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(342, 38);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 676);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Chat2 32feet.NET";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectBySelectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectByAddressToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showRadioInfoToolStripMenuItem;
+    }
+}
+
